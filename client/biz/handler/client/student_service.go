477180e600ada06ba3c95411e2f471ed409629db
Geternitier <211250036@smail.nju.edu.cn>
2023-07-13 15:25:35 +0800

    update server and client

--- client/biz/handler/client/student_service.go
+++ client/biz/handler/client/student_service.go
@@ -4,8 +4,14 @@ package client
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/connpool"
 
-	client "client/biz/model/client"
+	student2 "client/biz/model/client"
+	student "client/kitex_gen/client"
+	"client/kitex_gen/client/studentservice"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
@@ -14,30 +20,59 @@ import (
 // @router /add-student-info [POST]
 func Register(ctx context.Context, c *app.RequestContext) {
 	var err error
-	var req client.Student
+	var req student.Student
 	err = c.BindAndValidate(&req)
 	if err != nil {
 		c.String(consts.StatusBadRequest, err.Error())
 		return
 	}
 
-	resp := new(client.RegisterResp)
+	var opts []client.Option
+	opts = append(opts, client.WithHostPorts("127.0.0.1:9999"))
+	opts = append(opts, client.WithLongConnection(connpool.IdleConfig{MinIdlePerAddress: 10, MaxIdlePerAddress: 1000}))
 
-	c.JSON(consts.StatusOK, resp)
+	studentClient := studentservice.MustNewClient("student", opts...)
+
+	resp, err := studentClient.Register(context.Background(), &req)
+
+	if resp.Success {
+		c.String(consts.StatusOK, resp.Message)
+	} else {
+		c.String(consts.StatusBadRequest, errors.New("error: ID Already Existed").Error())
+	}
 }
 
 // Query .
 // @router /query [GET]
 func Query(ctx context.Context, c *app.RequestContext) {
 	var err error
-	var req client.QueryReq
+	var req student2.QueryReq
 	err = c.BindAndValidate(&req)
 	if err != nil {
 		c.String(consts.StatusBadRequest, err.Error())
 		return
 	}
 
-	resp := new(client.Student)
+	resp := new(student.Student)
+
+	var opts []client.Option
+	opts = append(opts, client.WithHostPorts("127.0.0.1:9999"))
+	opts = append(opts, client.WithLongConnection(connpool.IdleConfig{MinIdlePerAddress: 10, MaxIdlePerAddress: 1000}))
+
+	studentClient := studentservice.MustNewClient("student", opts...)
+
+	fmt.Printf("Got a Request for the information of ID: %v.\n", req.ID)
+
+	var request = student.QueryReq{req.ID}
+	resp, err = studentClient.Query(context.Background(), &request)
+
+	fmt.Println("Query ended.")
+
+	if err != nil {
+		fmt.Println(err)
+		c.String(consts.StatusBadGateway, err.Error())
+		return
+	}
 
 	c.JSON(consts.StatusOK, resp)
 }
--- server/main.go
+++ server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/cloudwego/kitex/server"
 	"log"
+	"net"
 	server0 "server/kitex_gen/server/studentservice"
 )
 
 func main() {
-	svr := server0.NewServer(new(StudentServiceImpl))
+	addr, _ := net.ResolveTCPAddr("tcp", ":9999")
+	svr := server0.NewServer(new(StudentServiceImpl), server.WithServiceAddr(addr))
 
 	err := svr.Run()
 
